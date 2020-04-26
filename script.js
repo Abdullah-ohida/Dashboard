fb0c0c4a4874972e6b6cac06eb4f1b390c17dd36
Abdullah-ohida <ohida2001@gmail.com>
2020-04-26 11:54:02 +0100

    add switch

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
- function myFunction(){
-  let element = document.body;
-   element.classList.toggle('dark-mode')
+const toggleSwitch = document.querySelector('.slider input[type = "range"]');
+// console.log(toggleSwitch);
+
+function switchTheme(e) {
+   val = toggleSwitch.value;
+   console.log(val);
+   if (val >= 1) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+
+   } else {
+      document.documentElement.setAttribute('data-theme', 'light')
+      localStorage.setItem('theme', 'light')
+   }
+
 }
+
+toggleSwitch.addEventListener('change', switchTheme, false);
